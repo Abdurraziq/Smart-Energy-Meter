895826749a1512ed298b21c34fb11937f1dc7351
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2018-12-21 05:16:20 +0800

    First Commit

---
+++ blynk_ver0.0.1.ino
@@ -0,0 +1,99 @@
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+#include <ACS712.h>
+
+// Auth Token Blynk App.
+char auth[] = "db18cc4e824c4424965f6ad5bf01c702";
+
+// WiFi credentials.
+char ssid[] = "S50D";
+char pass[] = "12345678";
+
+BlynkTimer timer;
+
+float Voltage;
+float Current;
+
+float Power;
+int PowerSet;
+float Energy;
+
+#define EspSerial Serial
+
+// ESP8266 baud rate:
+#define ESP8266_BAUD 115200
+
+ESP8266 wifi(&EspSerial);
+ACS712 sensor(ACS712_05B, A0);
+
+void setup()
+{
+	pinMode(13, OUTPUT);
+	pinMode(12, OUTPUT);
+
+	digitalWrite(12, LOW);
+	sensor.calibrate();
+
+	EspSerial.begin(ESP8266_BAUD);
+	delay(10);
+
+	Blynk.begin(auth, wifi, ssid, pass);
+	timer.setInterval(1000, calculate);
+	timer.setInterval(5000, sendData);
+}
+
+void loop()
+{
+	Blynk.run();
+	timer.run();
+}
+
+void calculate()
+{
+	Voltage = 230;
+
+	Current = sensor.getCurrentAC();
+	Current = round(Current * 10) / 10.0;
+
+	Power = Voltage * Current;
+	Power = round(Power * 10) / 10.0;
+
+	Energy += Power;
+
+	check();
+}
+
+void sendData()
+{
+	Blynk.virtualWrite(V0, Voltage);
+	Blynk.virtualWrite(V1, Current);
+	Blynk.virtualWrite(V2, Power);
+	Blynk.virtualWrite(V3, Energy);
+}
+
+BLYNK_CONNECTED()
+{
+	Blynk.syncAll();
+}
+
+BLYNK_WRITE(V4)
+{
+	PowerSet = param.asInt();
+}
+
+BLYNK_WRITE(V5)
+{
+	int pinValue = param.asInt();
+	digitalWrite(13, pinValue);
+	digitalWrite(12, pinValue);
+}
+
+void check()
+{
+	if (Power > PowerSet)
+	{
+		Blynk.virtualWrite(V5, LOW);
+		digitalWrite(13, LOW);
+		digitalWrite(12, LOW);
+	}
+}
